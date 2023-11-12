c08d6ead135d7257c7ca1b3a2222dda6905d28bc
ptnhut166 <138546114+ptnhut166@users.noreply.github.com>
2023-11-12 17:56:12 +0700

    commit

---
+++ app/src/main/java/com/example/salon/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.salon;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity
+{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_page);
+    }
+}
