09c847c2cb008bce3d8ba4bd7862cd661a51f5e7
ptnhut166 <138546114+ptnhut166@users.noreply.github.com>
2023-11-30 08:57:52 +0700

    commit

--- app/src/main/java/com/example/salon/HomeActivity.java
+++ app/src/main/java/com/example/salon/HomeActivity.java
@@ -14,6 +14,7 @@ import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class HomeActivity extends AppCompatActivity
@@ -31,6 +32,7 @@ public class HomeActivity extends AppCompatActivity
         Button btn_booking = findViewById(R.id.home_btn_booking);
         Button btn_shopping = findViewById(R.id.home_btn_shopping);
         Button btn_lookbook = findViewById(R.id.home_btn_lookbook);
+        Button btn_logout = findViewById(R.id.btn_logout);
         View.OnClickListener listenerBooking = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -47,10 +49,20 @@ public class HomeActivity extends AppCompatActivity
                 startActivity(intent);
             }
         };
+        View.OnClickListener listenerLogout = new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent();
+                intent.setClass(view.getContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        };
         //Gán Listener cho editbutton
         btn_booking.setOnClickListener(listenerBooking);
         btn_lookbook.setOnClickListener(listenerLookbook);
-
+        btn_logout.setOnClickListener(listenerLogout);
 
 
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
@@ -64,5 +76,6 @@ public class HomeActivity extends AppCompatActivity
                 return true;
             }
         });
+
     }
 }
--- app/src/main/java/com/example/salon/LoginActivity.java
+++ app/src/main/java/com/example/salon/LoginActivity.java
@@ -2,28 +2,98 @@ package com.example.salon;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class LoginActivity extends AppCompatActivity{
-    Button btn_login, btn_forgot, btn_signup;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-    protected void onCreate(Bundle savedInstanceState) {
+public class LoginActivity extends AppCompatActivity{
+    TextInputEditText edt_email, edt_password;
+    Button btn_login, btn_signup;
+    FirebaseAuth mAuth;
+
+
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_page);
 
-        btn_login=findViewById(R.id.btn_login);
-        btn_forgot=findViewById(R.id.btn_forgot);
+        mAuth = FirebaseAuth.getInstance();
+        edt_email = (TextInputEditText)findViewById(R.id.edt_email);
+        edt_password = (TextInputEditText)findViewById(R.id.edt_password);
+        btn_login = (Button)findViewById(R.id.btn_login);
         btn_signup=findViewById(R.id.btn_signup);
 
-        
 
+
+        btn_login.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                String email = String.valueOf(edt_email.getText());
+                String password = String.valueOf(edt_password.getText());
+
+                if(TextUtils.isEmpty(email))
+                {
+                    Toast.makeText(LoginActivity.this, "Please enter email address and password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password))
+                {
+                    Toast.makeText(LoginActivity.this, "Please enter email address and password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(LoginActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
         btn_signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent =new Intent(getApplicationContext(), RegisterActivity.class);
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
                 startActivity(intent);
                 finish();
             }
