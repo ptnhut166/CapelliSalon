25a74d531b39de215b9ee49f4bd189113190dfae
ptnhut166 <138546114+ptnhut166@users.noreply.github.com>
2023-12-01 18:08:48 +0700

    update

--- app/src/main/java/com/example/salon/LoginActivity.java
+++ app/src/main/java/com/example/salon/LoginActivity.java
@@ -8,7 +8,7 @@ import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
-
+import com.google.firebase.Firebase;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -32,6 +32,7 @@ public class LoginActivity extends AppCompatActivity{
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
+
         if(currentUser != null){
             Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
             startActivity(intent);
@@ -46,51 +47,49 @@ public class LoginActivity extends AppCompatActivity{
         setContentView(R.layout.login_page);
 
         mAuth = FirebaseAuth.getInstance();
-        edt_email = findViewById(R.id.edt_email);
-        edt_password = findViewById(R.id.edt_password);
+        edt_email = findViewById(R.id.edt_login_email);
+        edt_password = findViewById(R.id.edt_login_password);
         btn_login = findViewById(R.id.btn_login);
         btn_signup=findViewById(R.id.btn_signup);
 
 
 
-        btn_login.setOnClickListener(new View.OnClickListener()
-        {
+        btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                String email = String.valueOf(edt_email.getText());
-                String password = String.valueOf(edt_password.getText());
+            public void onClick(View v) {
+                loginUser();
+            }
 
-                if(TextUtils.isEmpty(email))
-                {
-                    Toast.makeText(LoginActivity.this, "Please enter email address and password", Toast.LENGTH_SHORT).show();
+            private void loginUser() {
+                String email, password;
+                email= String.valueOf(edt_email.getText());
+                password=String.valueOf(edt_password.getText());
+
+                if(TextUtils.isEmpty(email)){
+                    Toast.makeText(LoginActivity.this, "Please enter email", Toast.LENGTH_SHORT).show();
                     return;
                 }
-
-                if (TextUtils.isEmpty(password))
-                {
-                    Toast.makeText(LoginActivity.this, "Please enter email address and password", Toast.LENGTH_SHORT).show();
+                if(TextUtils.isEmpty(password)){
+                    Toast.makeText(LoginActivity.this, "Please enter password", Toast.LENGTH_SHORT).show();
                     return;
                 }
-
-                mAuth.signInWithEmailAndPassword(email, password)
-                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-                                    FirebaseUser user = mAuth.getCurrentUser();
-                                    Toast.makeText(LoginActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
-                                    Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
-                                    startActivity(intent);
-                                    finish();
-                                } else {
-                                    // If sign in fails, display a message to the user.
-                                    Toast.makeText(LoginActivity.this, "Authentication failed.",
-                                            Toast.LENGTH_SHORT).show();
-                                }
+                if(email.length()>0 && password.length()>0){
+                    mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+                                Toast.makeText(LoginActivity.this, "User logged in successfully", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+                            }else{
+                                Toast.makeText(LoginActivity.this, "Log in Error: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                             }
-                        });
+                        }
+                    });
+                }
+
             }
         });
+
         btn_signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -100,4 +99,5 @@ public class LoginActivity extends AppCompatActivity{
             }
         });
     }
+
 }
