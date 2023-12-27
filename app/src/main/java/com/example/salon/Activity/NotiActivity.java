c57c2bce74a8ad40f7239f0c78451a51c6c9d23a
Carliscoding <101809224+Carliscoding@users.noreply.github.com>
2023-12-27 21:27:01 +0700

    push_notification

--- app/src/main/java/com/example/salon/Activity/NotiActivity.java
+++ app/src/main/java/com/example/salon/Activity/NotiActivity.java
@@ -1,23 +1,21 @@
 package com.example.salon.Activity;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.salon.Domain.BookingInfo;
 import com.example.salon.Helper.NavigationManager;
 import com.example.salon.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
 import java.util.ArrayList;
 
 public class NotiActivity extends AppCompatActivity {
@@ -26,65 +24,92 @@ public class NotiActivity extends AppCompatActivity {
     private ArrayList<String> notificationList;
 
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.notifications_page);
+
         listnoti = findViewById(R.id.notificationListView);
-        Intent intent = getIntent();
-        BookingInfo bookingInfo = (BookingInfo) intent.getSerializableExtra("booking_info");
         notificationList = new ArrayList<>();
-
-        // Populate the ArrayList with booking information
-       // if (bookingInfo != null) {
-           // notificationList.add("Registration successful!"+"\n" +bookingInfo.getName() +"\n"+bookingInfo.getAddress() +"\n"+bookingInfo.getTime() + "\n" + bookingInfo.getNameStaff() + "\n" + bookingInfo.getPhoneNumber());
-            // Add other booking details as needed
-       // }
-
-        // Initialize ArrayAdapter and set it to the ListView
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, notificationList);
         listnoti.setAdapter(adapter);
-        DatabaseReference bookingRef = FirebaseDatabase.getInstance().getReference().child("bookings");
-        bookingRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                // Xử lý khi dữ liệu thay đổi
-                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                    BookingInfo bookingInfo = snapshot.getValue(BookingInfo.class);
-                    // Sử dụng bookingInfo ở đây
-                    if (bookingInfo != null) {
-                        // Tạo chuỗi thông tin từ đối tượng BookingInfo và thêm vào danh sách thông báo
-                        String info = "Registration successful!" + "\n" + bookingInfo.getName() + "\n" + bookingInfo.getAddress() + "\n" + bookingInfo.getTime() + "\n" + bookingInfo.getNameStaff() + "\n" + bookingInfo.getPhoneNumber();
-                        notificationList.add(info);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            String userID = user.getUid();
+            DatabaseReference userBookingRef = FirebaseDatabase.getInstance().getReference().child("userID").child(userID).child("InfoBooking");
+//            userBookingRef.addListenerForSingleValueEvent(new ValueEventListener() {
+//                @Override
+//                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                    if (dataSnapshot.exists()) {
+//                        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+//                            String address = snapshot.child("Address").getValue(String.class);
+//                            String locationName = snapshot.child("Location name").getValue(String.class);
+//                            String phoneNumber = snapshot.child("Phone number").getValue(String.class);
+//                            String staffName = snapshot.child("Staff name").getValue(String.class);
+//                            String time = snapshot.child("Time").getValue(String.class);
+//
+//                            address = address != null ? address : "No information";
+//                            locationName = locationName != null ? locationName : "No information";
+//                            phoneNumber = phoneNumber != null ? phoneNumber : "No information";
+//                            staffName = staffName != null ? staffName : "No information";
+//                            time = time != null ? time : "No information";
+//
+//                            String combinedData = "Registration successful!\nAddress: " + address +
+//                                    "\nLocation Name: " + locationName +
+//                                    "\nPhone Number: " + phoneNumber +
+//                                    "\nStaff Name: " + staffName +
+//                                    "\nTime: " + time;
+//
+//                            notificationList.add(combinedData);
+//                        }
+//                        adapter.notifyDataSetChanged();
+//                    } else {
+//                        Log.d("Firebase", "No data found at this path");
+//                    }
+//                }
+//
+//                @Override
+//                public void onCancelled(@NonNull DatabaseError databaseError) {
+//                    Log.d("Firebase", "Error: " + databaseError.getMessage());
+//                }
+//            });
+
+            userBookingRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    if (dataSnapshot.exists()) {
+                        Log.d("Firebase", "DataSnapshot exists: " + dataSnapshot.getValue());
+                        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                            Object data = snapshot.getValue();
+                            if (data != null) {
+                                String convertedData = data.toString();
+                                notificationList.add(convertedData);
+                            }
+
+
+                        }
+                        adapter.notifyDataSetChanged();
+                    } else {
+                        Log.d("Firebase", "No data found at this path");
                     }
                 }
-                adapter.notifyDataSetChanged();
-            }
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                // Xử lý khi có lỗi xảy ra
-            }
-        });
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+                    Log.e("Firebase", "onCancelled called: " + databaseError.getMessage());
+                }
+            });
 
-        BottomNavigationView bottomNav = findViewById(R.id.bnv_noti); // Thay R.id.bottom_navigation bằng ID của BottomNavigationView trong layout của bạn
-        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            BottomNavigationView bottomNav = findViewById(R.id.bnv_noti);
+            bottomNav.setOnNavigationItemSelectedListener(item -> {
                 int id = item.getItemId();
 
-                 if (id == R.id.action_home) {
-                    // Xử lý khi click vào Home
+                if (id == R.id.action_home) {
                     NavigationManager.navigateToHome(NotiActivity.this);
                 } else if (id == R.id.action_cart) {
-                    // Xử lý khi click vào Cart
                     NavigationManager.navigateToCart(NotiActivity.this);
-                }  else if (id == R.id.action_acc) {
-                    // Xử lý khi click vào Settings
-                     NavigationManager.navigateToProfile(NotiActivity.this);
                 }
-
                 return true;
-            }
-        });
+            });
+        }
     }
 }
